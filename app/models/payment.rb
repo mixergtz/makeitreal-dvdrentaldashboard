5516091f21703bb9f71f0f4c4f318a64953c1fad
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-11-24 17:38:01 -0500

    Add scope to see global sales by month

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,8 +2,22 @@ class Payment < ActiveRecord::Base
   self.table_name = 'payment'
   self.primary_key = 'payment_id'
 
-  #Returns array
-  #Payment.group("date_trunc('month', payment_date)").sum('amount')
-  #Returns ActiveRelation
-  #Payment.select("date_trunc('month', payment_date) as month, sum(amount) as ventas").group("month")
+  def self.global_sales_by_month
+    sales = select("date_trunc('month', payment_date) as month, sum(amount) as ventas").group("month")
+    result = {}
+    sales.each do |el|
+      result[el.month.strftime("%B/%Y")] = el.ventas
+    end
+    result
+  end
+
+  def self.sales_by_category
+    sales = select("date_trunc('month', payment_date) as month, sum(amount) as ventas").group("month")
+    result = {}
+    sales.each do |el|
+      result[el.month.strftime("%B/%Y")] = el.ventas
+    end
+    result
+  end
+
 end
